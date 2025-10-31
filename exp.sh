646619850d80e8355cbd39a1aec8c010cb12651c
Andrew <asolis@tacc.utexas.edu>
2025-10-30 17:34:24 -1000

    create output file for timestamp

--- exp.sh
+++ exp.sh
@@ -8,9 +8,12 @@ if [ ! -d "$graphs_dir" ]; then
   exit 1
 fi
 
+filename_timestamp=$(date +'%m-%d-%Y_%H-%M-%S').out
+echo "Timestamp for filename: $filename_timestamp"
+
 for file_path in "$graphs_dir"/*; do
   if [ -f "$file_path" ]; then
     echo "Processing file: $file_path"
-    cmake-build-debug/Karger "$file_path"
+    cmake-build-debug/Karger "$file_path" >> "$filename_timestamp" 2>&1
   fi
 done
